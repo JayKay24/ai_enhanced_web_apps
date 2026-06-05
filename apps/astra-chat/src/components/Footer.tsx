94782af62633c9732c2ca14dfe3073a25590a623
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-06-05 16:10:24 +0300

    chore: remove astra-chat application from the workspace
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/astra-chat/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <footer className="p-3 text-center border-t text-sm text-gray-500">© 2026 Astra Chat. All rights reserved.</footer>;
-};
-
-export default Footer;
